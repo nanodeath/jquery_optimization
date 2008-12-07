0a8b79d0045672de013769c72aafbb19f61703c3
Max Aller <nanodeath@gmail.com>
2008-12-06 19:55:46 -0800

    Maybe it shouldn't output text?

--- util/jsmin_my_js.rb
+++ util/jsmin_my_js.rb
@@ -2,13 +2,11 @@
 
 dir = File.dirname(__FILE__)
 js_files = Dir[dir + '/../support/**/*.js']
-puts js_files.inspect
 
 js_files.each do |f|
   out = f.split('.')
   next if out.include?("min")
   out.insert(-2, 'min')
   out = out.join('.')
-  puts out
   `#{dir}/jsmin.rb < #{f} > #{out}`
 end
