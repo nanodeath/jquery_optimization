3846e6155fd64f138627518636dc2b89bcf8d80c
Max Aller <nanodeath@gmail.com>
2008-12-06 20:26:58 -0800

    Revised script

--- util/jsmin_my_js.rb
+++ util/jsmin_my_js.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/ruby
 
-dir = File.dirname(__FILE__)
-js_files = Dir[dir + '/../support/**/*.js']
+dir = ARGV[0] || File.dirname(__FILE__)
+js_files = Dir[dir + '/support/**/*.js']
 
 js_files.each do |f|
   out = f.split('.')
   next if out.include?("min")
   out.insert(-2, 'min')
   out = out.join('.')
-  `#{dir}/jsmin.rb < #{f} > #{out}`
+  `#{dir}/util/jsmin.rb < #{f} > #{out}`
 end
+
